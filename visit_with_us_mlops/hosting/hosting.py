89abe1d40b313ddca3917ae12df53b234528ea44
Chandrachurh <66819207+Chandrachurh@users.noreply.github.com>
2026-03-08 12:10:44 +0530

    Update hosting.py

--- visit_with_us_mlops/hosting/hosting.py
+++ visit_with_us_mlops/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/content/visit_with_us_mlops/deployment",     # the local folder containing your files
+    folder_path="visit_with_us_mlops/deployment",     # the local folder containing your files
     repo_id="chandrachurhghosh/tourism-package-predictor-app",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
